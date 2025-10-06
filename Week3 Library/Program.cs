140a2b413ddf4cfa5d5856595a8a18bb6f6aba92
B00971548 <B00971548@M-MXB105-C06>
2025-10-06 15:00:16 +0100

    Created new classes

--- Week3 Library/Program.cs
+++ Week3 Library/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        //new object book with values
+        Book book = new Book();
+        book.Title = "C# for Beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+
+        //new object book1 with values
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        //output to console
+        book.DisplayInfo();
+        book1.DisplayInfo();
+    }
+}
