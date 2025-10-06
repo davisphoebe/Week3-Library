75081e621527f131a6c9c7761562d582c6961771
B00971548 <B00971548@M-MXB105-C06>
2025-10-06 15:10:38 +0100

    updated with access modifiers

---
+++ Week3 Library/.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Week3_Library;
+
+    static void Main(string[] args)
+    {
+        //new objects with values
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+        Book book1 = new Book("C# Methods and classes", "Microsoft", "55667778");
+
+
+        //output to console
+        book.DisplayInfo();
+        book1.DisplayInfo();
+    }
---
+++ Week3 Library/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        //constructor
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
+}
