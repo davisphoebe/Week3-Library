9e1c0a7b87e87739809be12caea940bcbbc69fb3
B00971548 <B00971548@M-MXB105-C06>
2025-10-06 15:04:30 +0100

    added a constructor

--- Week3 Library/Program.cs
+++ Week3 Library/Program.cs
@@ -7,6 +7,15 @@ class Book
     string Author;
     string ISBN;
 
+
+    //constructor
+    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+
     void DisplayInfo()
     {
         Console.WriteLine($"Book title: {Title}");
@@ -17,18 +26,10 @@ class Book
 
     static void Main(string[] args)
     {
-        //new object book with values
-        Book book = new Book();
-        book.Title = "C# for Beginners";
-        book.Author = "BillGates";
-        book.ISBN = "12345678";
+        //new objects with values
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+        Book book1 = new Book("C# Methods and classes", "Microsoft", "55667778");
 
-
-        //new object book1 with values
-        Book book1 = new Book();
-        book1.Title = "C# Methods and classes";
-        book1.Author = "Microsoft";
-        book1.ISBN = "55667778";
 
         //output to console
         book.DisplayInfo();
